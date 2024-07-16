70e66936b980b9f30d66cc706804fc92e0e73ced
LeonnelTang <155005203+LeonnelTang@users.noreply.github.com>
2024-07-16 18:49:12 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -1,28 +1,13 @@
 import pandas as pd
 
-# 创建一个包含 None 和 {None, None} 的示例 DataFrame
-data = {
-    'city': ['上海·静安·静安寺', '上海·张江', '上海·虹口·北外滩',
-             '上海·浦东·唐镇', '上海·徐汇·斜土路'],
-    'location_data': [None, {'lat': 31.23, 'lng': 121.48}, {None, None}, None, {'lat': 31.19, 'lng': 121.44}]
-}
+# 读取CSV文件
+csv_file = 'combined_processed_data.csv'
+df = pd.read_csv(csv_file)
 
-df = pd.DataFrame(data)
+# 保存为Excel文件
+excel_file = '上海市互联网行业招聘数据集.xlsx'
+df.to_excel(excel_file, index=False)
+print(f"CSV文件已成功转换为Excel文件：{excel_file}")
 
-print("原始 DataFrame:")
-print(df)
-
-# 使用 dropna 函数过滤掉包含 None 的行
-df_cleaned = df.dropna(subset=['location_data'])
-
-print("\n使用 dropna 过滤后的 DataFrame:")
-print(df_cleaned)
-
-# 使用 apply 函数来处理 location_data 列，将 {None, None} 替换为 None
-df['location_data'] = df['location_data'].apply(lambda x: None if isinstance(x, set) and None in x else x)
-
-# 使用 dropna 函数再次过滤掉包含 None 的行
-df_cleaned = df.dropna(subset=['location_data'])
-
-print("\n处理 {None, None} 并使用 dropna 过滤后的 DataFrame:")
-print(df_cleaned)
+df.to_csv('上海市互联网行业招聘数据集.csv', index=False)
+print(f"CSV文件已成功转换为CSV文件：{'上海市互联网行业招聘数据集.csv'}")
